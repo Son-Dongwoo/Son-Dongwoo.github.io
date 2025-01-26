dfbfd2a0b9db6734215447332f9311e74535c199
Son-Dongwoo <sondongwoo2024@gmail.com>
2025-01-27 05:41:14 +0900

    upload deploy_window.bat

--- deploy_window.bat
+++ deploy_window.bat
@@ -12,12 +12,11 @@ cd public
 :: Add changes to git in the public folder
 git add .
 
-:: Commit changes in the public folder
-set msg="Rebuilding site on %date% %time%"
-if "%~1" NEQ "" (
-  set msg="%~1"
-)
-git commit -m %msg%
+:: Ask for commit message for public folder
+echo Enter commit message for gh-pages branch \(Press ENTER for default message\):
+set /p msg_public="> "
+if "%msg_public%"=="" set msg_public=Rebuilding site on %date% %time%
+git commit -m "%msg_public%"
 
 :: Push changes to the gh-pages branch
 git push origin gh-pages
@@ -28,12 +27,11 @@ cd ..
 :: Add changes in the root directory
 git add .
 
-:: Commit changes in the root directory
-set msg="Rebuilding site on %date% %time%"
-if "%~1" NEQ "" (
-  set msg="%~1"
-)
-git commit -m %msg%
+:: Ask for commit message for main branch
+echo Enter commit message for main branch \(Press ENTER for default message\):
+set /p msg_main="> "
+if "%msg_main%"=="" set msg_main=Rebuilding site on %date% %time%
+git commit -m "%msg_main%"
 
 :: Push changes to the main branch
 git push origin main
